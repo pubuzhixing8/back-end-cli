5ac43833c611674aba99b3e8333a23651df3a716
pubuzhixing8 <pubuzhixing@gmail.com>
2024-05-28 16:20:09 +0800

    feat: add clean cli

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -2,12 +2,14 @@ import { toCamelCase } from '@bhouston/common-lib';
 import { createRequire } from 'module';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import fs from 'fs';
 
 const require = createRequire(import.meta.url);
 const packageInfo = require('../package.json');
 
 type CommandLineArgs = {
   error: boolean;
+  path: string;
 };
 
 export const main = async () => {
@@ -18,11 +20,35 @@ export const main = async () => {
         type: 'boolean',
         default: false,
         description: 'Throw an error'
+      },
+      path: {
+        type: 'string',
+        default: ''
       }
     }).argv) as CommandLineArgs;
 
   if (argv.error) {
     throw new Error();
   }
-  console.log(toCamelCase('hello World!'));
+
+  if (!argv.path) {
+    throw new Error('can not resolve path');
+  }
+
+  const path = argv.path;
+  const dotIndex = path.lastIndexOf('.');
+  const newPath =
+    path.slice(0, dotIndex) + `_clean.` + path.slice(dotIndex + 1);
+
+  const htmlContent = fs.readFileSync(`${argv.path}`, 'utf-8');
+
+  const aTagRegex = /<a\b([^>]*)>([\s\S]*?)<\/a>/g;
+
+  const newHtmlContent = htmlContent.replace(
+    aTagRegex,
+    (match, attributes, innerText) => {
+      return `<a${attributes}>[xxx]</a>`;
+    }
+  );
+  fs.writeFileSync(newPath, newHtmlContent);
 };
